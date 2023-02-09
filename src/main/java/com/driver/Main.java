55107f3d25a0d330ea4029830dc41c386d4be479
Anjali s <savantanjali0@gmail.com>
2023-02-09 08:18:51 +0530

    method overloding commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+public static class Product{
+    public int product(int x, int y) {}
+    public int product(int x, int y, int z) {}
+    public double product(double x, double y) {}
+}
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product();
+        p.product();
+        p.product();
+    }
 }
